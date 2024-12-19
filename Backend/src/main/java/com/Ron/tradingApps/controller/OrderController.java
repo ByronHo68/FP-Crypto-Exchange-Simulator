ace33251c377970773cd8f951838b37dbd3c0a3c
ByronHo68 <137786614+ByronHo68@users.noreply.github.com>
2024-12-19 13:45:02 +0800

    added principal for security

--- Backend/src/main/java/com/Ron/tradingApps/controller/OrderController.java
+++ Backend/src/main/java/com/Ron/tradingApps/controller/OrderController.java
@@ -5,12 +5,15 @@ import com.Ron.tradingApps.dto.response.OrderResponseDTO;
 import com.Ron.tradingApps.service.BalanceCheckService;
 import com.Ron.tradingApps.service.OrderService;
 import com.Ron.tradingApps.service.PriceCheckService;
+import com.Ron.tradingApps.service.UsernameCheckerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -24,10 +27,21 @@ public class OrderController {
     private BalanceCheckService balanceCheckService;
     @Autowired
     private PriceCheckService priceCheckService;
+    @Autowired
+    private UsernameCheckerService usernameCheckerService;
 
 
     @PostMapping
-    public ResponseEntity<OrderResponseDTO> createOrder(@RequestBody OrderRequestDTO orderRequestDTO) {
+    public ResponseEntity<OrderResponseDTO> createOrder(@RequestBody OrderRequestDTO orderRequestDTO, Principal principal) {
+        JwtAuthenticationToken token = (JwtAuthenticationToken) principal;
+        String username = token.getTokenAttributes().get("name").toString();
+
+        boolean isUsernameValid = usernameCheckerService.checkUsername(username, orderRequestDTO);
+
+        if (!isUsernameValid) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
         try {
             OrderResponseDTO orderResponse = orderService.createOrder(orderRequestDTO);
             priceCheckService.checkAndExecuteTransaction(orderResponse);
@@ -41,8 +55,18 @@ public class OrderController {
     }
 
 
+
     @DeleteMapping("/{orderId}")
-    public ResponseEntity<Void> deleteOrder(@PathVariable int orderId) {
+    public ResponseEntity<Void> deleteOrder(@PathVariable int orderId, Principal principal) {
+        JwtAuthenticationToken token = (JwtAuthenticationToken) principal;
+        String username = token.getTokenAttributes().get("name").toString();
+
+        boolean isUsernameValid = usernameCheckerService.checkOrderId(username, orderId);
+
+        if (!isUsernameValid) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
         try {
             log.info("Attempting to delete order with ID: {}", orderId);
             orderService.deleteOrder(orderId);
@@ -58,7 +82,16 @@ public class OrderController {
     }
 
     @GetMapping("/trader/{userId}")
-    public ResponseEntity<List<OrderResponseDTO>> getAllOrdersForTrader(@PathVariable String userId) {
+    public ResponseEntity<List<OrderResponseDTO>> getAllOrdersForTrader(@PathVariable String userId, Principal principal) {
+        JwtAuthenticationToken token = (JwtAuthenticationToken) principal;
+        String username = token.getTokenAttributes().get("name").toString();
+
+        boolean isUsernameValid = usernameCheckerService.checkUid(username, userId);
+
+        if (!isUsernameValid) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
         try {
             List<OrderResponseDTO> orders = orderService.findAllOrdersByTrader(userId);
             return new ResponseEntity<>(orders, HttpStatus.OK);
@@ -68,7 +101,11 @@ public class OrderController {
     }
 
     @GetMapping("/trader/{userId}/pending")
-    public ResponseEntity<List<OrderResponseDTO>> getPendingOrdersForTrader(@PathVariable String userId) {
+    public ResponseEntity<List<OrderResponseDTO>> getPendingOrdersForTrader(@PathVariable String userId, Principal principal) {
+        JwtAuthenticationToken token = (JwtAuthenticationToken) principal;
+        String username = token.getTokenAttributes().get("name").toString();
+
+        boolean isUsernameValid = usernameCheckerService.checkUid(username, userId);
         try {
             List<OrderResponseDTO> pendingOrders = orderService.findPendingOrdersByTrader(userId);
             return new ResponseEntity<>(pendingOrders, HttpStatus.OK);
---
+++ Backend/src/main/java/com/Ron/tradingApps/service/UsernameCheckerService.java
@@ -0,0 +1,57 @@
+package com.Ron.tradingApps.service;
+
+import com.Ron.tradingApps.dto.request.OrderRequestDTO;
+import com.Ron.tradingApps.model.Order;
+import com.Ron.tradingApps.model.Trader;
+import com.Ron.tradingApps.repository.OrderRepository;
+import com.Ron.tradingApps.repository.TraderRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UsernameCheckerService {
+
+    @Autowired
+    private TraderRepository traderRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public boolean checkUsername(String username, OrderRequestDTO orderRequestDTO){
+
+        Trader trader = traderRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Trader not found by id " + username
+                ));
+        int traderId = orderRequestDTO.getTraderId();
+        return trader.getId().equals(traderId);
+    }
+
+    public boolean checkOrderId(String username, int orderId){
+
+        Trader trader = traderRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Product not found by id " + username
+                ));
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new ResourceNotFoundException(
+                "Order not found by id " + orderId
+        ));
+        return trader.getId().equals(order.getTrader().getId());
+    }
+
+    public boolean checkUid(String username, String userId){
+
+        Trader trader = traderRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Trader not found by id " + username
+                ));
+
+        Trader traderDataBase = traderRepository.findByUserId(userId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Trader not found by uid " + userId
+                ));
+        return trader.getId().equals(traderDataBase.getId());
+    }
+}
