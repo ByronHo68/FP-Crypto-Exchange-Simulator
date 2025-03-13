f38d66c9b6009439d4140028098784802c047235
ByronHo68 <137786614+ByronHo68@users.noreply.github.com>
2025-03-13 19:15:19 +0800

    clean code

--- Backend/src/main/java/com/Ron/tradingApps/model/type/Cryptocurrencies.java
+++ Backend/src/main/java/com/Ron/tradingApps/model/type/Cryptocurrencies.java
@@ -1,16 +1,17 @@
 package com.Ron.tradingApps.model.type;
 
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 public class Cryptocurrencies {
     private Type type;
+    @Getter
     public enum Type{
         BTCUSDT,
         ETHUSDT;
         /*SOLUSDT;*/
 
         private String type;
-        public String getType(){return type;}
     }
 }
